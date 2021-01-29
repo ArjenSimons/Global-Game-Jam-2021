fe686b5075a4fa5530cf3f3e384082ae15045a2f
dyzalonius <dyzalonius@gmail.com>
2021-01-29 20:08:56 +0100

    Add fields to form

--- Assets/Scripts/Printer.cs
+++ Assets/Scripts/Printer.cs
@@ -16,26 +16,28 @@ public class Printer : MonoBehaviour
 
     private void Start()
     {
-        Print("Yeehaw");
+        Print("Test", "Test print");
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            Print("typeofTeam");
+            Print("John Doe", "Is anyone reading this");
         }
     }
 
-    public void Print(string text)
+    public void Print(string name, string text)
     {
         Form form = Instantiate(prefabForm, printSpawn.position, transform.rotation).GetComponent<Form>();
-        form.SetText(text);
+        form.SetText(name, "", "", text);
         LeanTween.move(form.gameObject, printSpawn.position - printSpawn.forward * tweenDistance, tweenDuration).setOnComplete(() => form.EnablePaper());
     }
 
     public void Print(LostItem lostItem)
     {
-
+        Form form = Instantiate(prefabForm, printSpawn.position, transform.rotation).GetComponent<Form>();
+        form.SetText("John Doe", lostItem.ItemType.DisplayName, lostItem.ItemColor.DisplayName);
+        LeanTween.move(form.gameObject, printSpawn.position - printSpawn.forward * tweenDistance, tweenDuration).setOnComplete(() => form.EnablePaper());
     }
 }
