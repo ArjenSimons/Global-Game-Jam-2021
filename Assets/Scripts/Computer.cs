2cc7dfce170d073697e46524bd8cdf41a148675b
Arjen Simons <arjen.simons@gmail.com>
2021-01-30 02:04:01 +0100

    Change score display on pc screen

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -3,14 +3,33 @@ using UnityEngine;
 
 public class Computer : MonoBehaviour
 {
+    [Header("Prefab references")]
     [SerializeField]
     private TMP_Text textTime = null;
 
     [SerializeField]
-    private TMP_Text textForms = null;
+    private TMP_Text textCorrectForms = null;
 
-    public void SetFormsText(int amountCompleted)
+    [SerializeField]
+    private TMP_Text textIncorrectForms = null;
+
+    [Header("Scene references")]
+    private ScoresManager scoreManager = null;
+
+    [Header("Channel Broadcasting on")]
+    [SerializeField]
+    private EmptyChannel scoresUpdatedChannel = null;
+
+    private void Start()
     {
-        textForms.text = amountCompleted + " forms completed";
+        SetTexts();
+        scoresUpdatedChannel.OnEventRaised += SetTexts;
+    }
+
+    private void SetTexts()
+    {
+        textCorrectForms.text = scoreManager.CorrectForms + " forms completed";
+        textIncorrectForms.text = scoreManager.IncorrectForms + " mistakes";
+
     }
 }
