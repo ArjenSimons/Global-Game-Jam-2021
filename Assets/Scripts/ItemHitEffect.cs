6c18e4a3c8e27e9ed92d098a0b14b6f735e514a2
Arjen Simons <arjen.simons@gmail.com>
2021-01-30 18:36:03 +0100

    Add start for RigidHitSound effect

---
+++ Assets/Scripts/ItemHitEffect.cs
@@ -0,0 +1,31 @@
+using BWolf.Utilities.AudioPlaying;
+using UnityEngine;
+
+public class ItemHitEffect : MonoBehaviour
+{
+    [Header("Sound")]
+    [SerializeField]
+    private AudioCueSO audioCue = null;
+
+    [SerializeField]
+    private AudioConfigurationSO config = null;
+
+    [Header("Channel Broadcasting on")]
+    [SerializeField]
+    private AudioRequestChannelSO channel = null;
+
+    void OnCollisionEnter(Collision hit)
+    {
+        Debug.Log(hit.relativeVelocity.magnitude);
+        if (hit.relativeVelocity.magnitude > 2)
+        {
+            float tempVol = config.volume;
+
+            config.volume = tempVol / 60 * Mathf.Min(hit.relativeVelocity.magnitude, 5);
+
+            channel.RaiseEvent(config, audioCue, transform.position);
+
+            config.volume = tempVol;
+        }
+    }
+}
